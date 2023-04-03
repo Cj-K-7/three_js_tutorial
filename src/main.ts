fd177884ee557a70768d192ba2f1aad343e89e0f
Cj-K-7 <kllt7059@gmail.com>
2023-04-03 22:29:04 +0900

    helpers guide + torus

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,63 @@
-import * as three from "three";
+import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 function main() {
-  // const app = document.querySelector<HTMLDivElement>("#app")!;
-
-  const scene = new three.Scene();
-  const camera = new three.PerspectiveCamera(
-    75, //fov — Camera frustum vertical field of view. Default value is 50.
-    window.innerWidth / window.innerHeight, //aspect — Camera frustum aspect ratio. Default value is 1.
-    0.1, //near — Camera frustum near plane. Default value is 0.1.
-    1000 //far — Camera frustum far plane. Default value is 2000.
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75, //filed of view
+    window.innerWidth / window.innerHeight, //aspect ratio
+    0.1, //view frustrum near
+    1000 // view frustrum far
   );
 
-  const renderer = new three.WebGLRenderer();
+  const renderer = new THREE.WebGLRenderer({
+    canvas: document.getElementById("webgl") as HTMLCanvasElement,
+    antialias: true,
+  });
+  renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
 
-  const geometry = new three.BoxGeometry(1, 1, 1);
-  const material = new three.MeshBasicMaterial({ color: 0x00ff00 });
-  const cube = new three.Mesh(geometry, material);
+  camera.position.setZ(50);
+
+  const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+  const material = new THREE.MeshStandardMaterial({
+    color: 0xaaaaaa,
+  });
+  const torus = new THREE.Mesh(geometry, material);
+
+  scene.add(torus);
+
+  const pointLight = new THREE.PointLight(0xffffff);
+  const pointLightHelper = new THREE.PointLightHelper(pointLight);
+  const ambientLight = new THREE.AmbientLight(0xffffff);
+  const gridHelper = new THREE.GridHelper(1000, 100);
+  pointLight.position.set(10, 3, 5);
+  scene.add(pointLight, pointLightHelper, ambientLight, gridHelper);
 
   const controls = new OrbitControls(camera, renderer.domElement);
 
-  scene.add(cube);
+  function addStar() {
+    const geometry = new THREE.SphereGeometry(1, 10, 10);
+    const material = new THREE.MeshStandardMaterial({
+      color: 0xffffff,
+    });
+    const star = new THREE.Mesh(geometry, material);
 
-  camera.position.z = 10;
-  controls.update();
+    const [x, y, z] = Array(3)
+      .fill(null)
+      .map(() => THREE.MathUtils.randFloatSpread(500));
+    star.position.set(x, y, z);
+    scene.add(star);
+  }
+
+  Array(200).fill(null).forEach(addStar);
 
   function animate() {
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.01;
+    torus.rotation.z += 0.01;
 
     controls.update();
 
